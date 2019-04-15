f88a04f43f02e2ed96bc62ae4a706eafddffe003
Colin <colin.reitman87@gmail.com>
2019-04-15 17:16:44 -0400

    backend/frontend auth complete

---
+++ frontend/components/app.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import GreetingContainer from './greeting/greeting_container';
+import { Route, Switch } from 'react-router-dom';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+
+import SignUpFormContainer from './session_form/signup_form_container';
+import LogInFormContainer from './session_form/login_form_container';
+
+const App = () => (
+  <div>
+  <header>
+    <GreetingContainer />
+  </header>
+    <Switch>
+      <AuthRoute exact path="/login" component={LogInFormContainer} />
+      <AuthRoute exact path="/signup" component={SignUpFormContainer} />
+    </Switch>
+  </div>
+  
+);
+
+export default App;
